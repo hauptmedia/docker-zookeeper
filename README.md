04df21f13150ca0509f850af5a5ee91c7c664f1f
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-29 15:04:20 +0200

    added SERVER_ID

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ Example run:
 
 ```bash
 docker run -d \
+-e SERVER_ID=1 \
 -e TICK_TIME=2000 \
--e DATA_DIR=/var/lib/zookeeper/ \
+-e DATA_DIR=/var/lib/zookeeper/data \
+-e DATA_LOG_DIR=/var/lib/zookeeper/log \
 -e CLIENT_PORT=2181 \
 -e INIT_LIMIT=2000 \
 -e SYNC_LIMIT=2 \
 -e SERVER= zoo1:2888:3888,zoo2:2888:3888,zoo3:2888:3888 \
--v /volumes/zookeeper:/var/lib/zookeeper \
+-v /mnt/hdd/1/zookeeper/data:/var/lib/zookeeper/data \
+-v /mnt/hdd/2/zookeeper/log:/var/lib/zookeeper/log \
 hauptmedia/zookeeper
 ```
+
+For more information about the configuration parameters have a look at http://zookeeper.apache.org/doc/trunk/zookeeperAdmin.html
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,16 +3,38 @@ set -e
 
 ZOO_CFG=/opt/zookeeper/conf/zoo.cfg
 
+if [ -z "$SERVER_ID" ]; then
+	echo >&2 'error:  missing SERVER_ID'
+	echo >&2 '  Did you forget to add -e SERVER_ID=... ?'
+	exit 1
+fi
+
 TICK_TIME=${TICK_TIME:-2000}
-DATA_DIR=${DATA_DIR:-/var/lib/zookeeper/}
+DATA_DIR=${DATA_DIR:-/var/lib/zookeeper/data}
 CLIENT_PORT=${CLIENT_PORT:-2181}
 INIT_LIMIT=${INIT_LIMIT:-2000}
 SYNC_LIMIT=${SYNC_LIMIT:-2}
 
 echo "tickTime=${TICK_TIME}" >${ZOO_CFG}
+
 echo "dataDir=${DATA_DIR}" >>${ZOO_CFG}
+
+if [ ! -d "$DATA_DIR" ]; then
+	mkdir -p $DATA_DIR
+fi
+
+if [ -n "$DATA_LOG_DIR" ]; then
+	echo "dataLogDir=${DATA_LOG_DIR}" >>${ZOO_CFG}
+
+	if [ ! -d "$DATA_LOG_DIR" ]; then
+		mkdir -p $DATA_LOG_DIR
+	fi
+fi
+
 echo "clientPort=${CLIENT_PORT}" >>${ZOO_CFG}
+
 echo "initLimit=${INIT_LIMIT}" >>${ZOO_CFG}
+
 echo "syncLimit=${SYNC_LIMIT}" >>${ZOO_CFG}
 
 if [ -n "$SERVER" ]; then
@@ -24,6 +46,6 @@ if [ -n "$SERVER" ]; then
 fi
 
 
-
+echo $SERVER_ID >${DATA_DIR}/myid
 
 exec "$@"
