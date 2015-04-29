cef769445c5f4dace7114043d99563288e1752c6
Julian Haupt <julian.haupt@hauptmedia.de>
2015-04-29 14:37:07 +0200

    write zoo.cfg file on startup

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # docker-zookeeper
+
+Example run:
+
+```bash
+docker run -d \
+-e TICK_TIME=2000 \
+-e DATA_DIR=/var/lib/zookeeper/ \
+-e CLIENT_PORT=2181 \
+-e INIT_LIMIT=2000 \
+-e SYNC_LIMIT=2 \
+-e SERVER= zoo1:2888:3888,zoo2:2888:3888,zoo3:2888:3888 \
+-v /volumes/zookeeper:/var/lib/zookeeper \
+hauptmedia/zookeeper
+```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 set -e
 
+ZOO_CFG=/opt/zookeeper/conf/zoo.cfg
+
+TICK_TIME=${TICK_TIME:-2000}
+DATA_DIR=${DATA_DIR:-/var/lib/zookeeper/}
+CLIENT_PORT=${CLIENT_PORT:-2181}
+INIT_LIMIT=${INIT_LIMIT:-2000}
+SYNC_LIMIT=${SYNC_LIMIT:-2}
+
+echo "tickTime=${TICK_TIME}" >${ZOO_CFG}
+echo "dataDir=${DATA_DIR}" >>${ZOO_CFG}
+echo "clientPort=${CLIENT_PORT}" >>${ZOO_CFG}
+echo "initLimit=${INIT_LIMIT}" >>${ZOO_CFG}
+echo "syncLimit=${SYNC_LIMIT}" >>${ZOO_CFG}
+
+if [ -n "$SERVER" ]; then
+	i=1
+	for server in $(echo $SERVER | tr ',' ' '); do
+		echo server.${i}=$server >>${ZOO_CFG}
+		let "i = i + 1"
+	done
+fi
+
+
+
+
 exec "$@"
